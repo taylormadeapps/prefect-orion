5477d7d6c2e78620bb677e2884db836eedf23a37
taylormadeapps <ttaylor@encircle.co.uk>
2023-04-11 12:39:29 +0100

    make it work!

--- flows/test_flow/deployment.py
+++ flows/test_flow/deployment.py
@@ -7,12 +7,12 @@ from prefect.infrastructure import DockerContainer
 minio_block = RemoteFileSystem(
     basepath="s3://prefect-flows/test_flow",
     key_type="hash",
-    settings=dict(
-        use_ssl = False,
-        key = "blablabla",
-        secret = "blablabla",
-        client_kwargs = dict(endpoint_url = "http://minio:9000")
-    ),
+    settings={
+        "use_ssl" : False,
+        "key" :"blablabla",
+        "secret" : "blablabla",
+        "client_kwargs" : {"endpoint_url": "http://promethea2.local:9000"}
+    },
 )
 minio_block.save("minio", overwrite=True)
 
@@ -21,16 +21,16 @@ deployment = Deployment.build_from_flow(
     name="docker-example",
     flow=my_docker_flow,
     storage=RemoteFileSystem.load('minio'),
-    infrastructure=DockerContainer(
-        image = 'prefect-orion:2.4.5',
-        image_pull_policy = 'IF_NOT_PRESENT',
-        networks = ['prefect'],
-        env = {
-            "USE_SSL": False,
-            "AWS_ACCESS_KEY_ID": "blablabla",
-            "AWS_SECRET_ACCESS_KEY": "blablabla",
-            "ENDPOINT_URL": 'http://minio:9000',
-        }
-    ),
+    #infrastructure=DockerContainer(
+    #    image = 'prefect-orion:2.4.5',
+    #    image_pull_policy = 'IF_NOT_PRESENT',
+    #    networks = ['prefect'],
+    #    env = {
+    #        "USE_SSL": False,
+    #        "AWS_ACCESS_KEY_ID": "blablabla",
+    #        "AWS_SECRET_ACCESS_KEY": "blablabla",
+    #        "ENDPOINT_URL": 'http://promethea2.local:9000',
+    #    }
+    #),
 )
 deployment.apply()
--- flows/test_flow/test_flow.py
+++ flows/test_flow/test_flow.py
@@ -5,3 +5,6 @@ from prefect import flow, get_run_logger
 def my_docker_flow():
     logger = get_run_logger()
     logger.info("Hello from Docker!")
+
+if __name__=="__main__":
+ my_docker_flow()
